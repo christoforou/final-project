b82ec4ac01fb67c58c7c105bafdfd398dc4f487e
smc9000 <55109395+smc9000@users.noreply.github.com>
2019-12-15 07:51:31 -0600

    Create course_search.js

---
+++ course_search.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let routes = require('./routes');
+
+
+let pug = require("pug");
+
+let course_search = express();
+
+
+course_search.set('view engine','pug')
+
+
+course_search.use('/',routes);
+
+
+course_search.get('/',function(req,res){
+    res.render('index');
+})
+
+course_search.listen(3000,function(){
+    console.log('Listening on port 3000!')
+})
